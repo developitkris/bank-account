faf8ffefdeb9546bf5b7575068b3bcb3015e5511
Kristi Hwang and Rio Atmadja <kristi@>
2018-04-03 15:39:31 -0700

    add user by register button, update standing balance by withdrawing and/or depositing

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,61 @@
 //business logic
-function Account (name,initDeposit){
+var nextAccountNumber = parseInt( Math.random() * 90000 );
+
+function Account(name,initDeposit) {
   this.name = name;
   this.initDeposit = initDeposit;
+  this.accountNumber = nextAccountNumber+=10;
 
 }
-
 Account.prototype.Deposit = function(amount) {
-  this.initDeposit += amount;
-  return this.initDeposit;
+  return this.initDeposit += amount;
 }
 
 Account.prototype.Withdrawal = function(amount) {
-  this.initDeposit -= amount;
-  return this.initDeposit;
+  return this.initDeposit -= amount;
 }
 
 Account.prototype.AccountNumber = function() {
-  return parseInt( Math.random() * 90000 ).toString();
+  return this.accountNumber;
 }
 //User logic
 $(document).ready(function(){
 
-  $("form#create").submit(function(event){ // submit the form
-    event.preventDefault();
 
+
+  $("button#register").click(function(){ // submit the form
     // getting user input
     var name = $(".name").val();
-    var number = parseFloat( $("#initDeposit").val() );
+    var number = parseInt($("#initDeposit").val());
 
-    var users = new Account(name,initDeposit); //instantiate a new user account
+    console.log(number);
+
+    var users = new Account(name,number); //instantiate a new user account
 
     // show the newly created account
     $("#Registered").show();
     $(".Name").text(name);
-    $("#accountNum").text(users.AccountNumber() + 10);
+    $("#accountNum").text(users.AccountNumber());
+    console.log(users.initDeposit);
+    $("#balance").text(users.initDeposit);
+
+    $("button#update").click(function(){
+    var deposit= parseInt($("#deposit").val());
+    var withdrawal= parseInt($("#withdrawal").val());
+      //deposit
+      if( parseInt($("#acctNum").val())  === users.AccountNumber() ) {
+        $("#balance").val(users.Deposit(deposit));
+      } else {
+        alert("Please enter a valid account number");
+      };
+
+      if(parseInt($("#acctNum").val()) === users.AccountNumber() ) {
+        $("#balance").val(users.Withdrawal(withdrawal));
+      } else {
+        alert("Please enter a valid account number");
+      };
+       
+    });
   });
+
 })
